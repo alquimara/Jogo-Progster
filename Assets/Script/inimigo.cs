0104c9ab572631d61eb0f2740d8310459973fb72
Alquimara Braz Alves <alquimara.a.braz@gmail.com>
2020-03-16 01:48:48 -0300

    Adicionado a movimentação e o cena de game over

---
+++ Assets/Script/inimigo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inimigo : MonoBehaviour
+{
+
+	private Vector2 pos1 = new Vector2(-7.2F,0);
+     	private Vector2 pos2 = new Vector2(-5.7F,0);
+     	public float speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.Lerp (pos1, pos2, Mathf.PingPong(Time.time*speed, 1.0f));
+    }
+}
--- Assets/Script/movimento.cs
+++ Assets/Script/movimento.cs
@@ -1,37 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class movimento : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float velocidade;
-	public Vector2 posiçao;
+    	public float velocidade;
+	public float posicao;
 	private Rigidbody2D personagem;
 	public float velocidadeY;
+	public GameObject other;
+	public Vector2 posicaoinicial;
+	
+
 
 	// Use this for initialization
 	void Start () {
 		personagem = GetComponent<Rigidbody2D> ();
+		print(transform.position.x);
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.D)) {
-			transform.Translate (Vector2.right * velocidade * posiçao.x * Time.deltaTime);
+			transform.Translate(Vector2.right* 1.45F);
+			print(transform.position.x);
+			
+
+
+		
 		}
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			personagem.AddForce (transform.up * velocidadeY );
+			personagem.AddForce (transform.up * 5 );
+			
 
 		
 		
 		}
 	}
-	void OnTriggerEnter2D (Collider2D Obj){
+void OnTriggerEnter2D (Collider2D Obj){
 		if(Obj.gameObject.tag == "semente"){
 			Destroy (Obj.gameObject);
 		}
+		if(Obj.gameObject.tag == "cobra"){
+		Destroy(other);	
+		SceneManager.LoadScene("game over");
+		
+			
+
+}
 			
 		}
+	
 }
