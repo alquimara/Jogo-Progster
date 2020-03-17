153ac64f07db36011c5f2695728823e25e0dea93
Alquimara Braz Alves <alquimara.a.braz@gmail.com>
2020-03-17 04:27:20 -0300

    Comecando ação de arrastar peças

---
+++ Assets/Script/DragHandLer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandLer : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    public static GameObject pecaDragging;
+    Vector3 startPosition;
+	Transform startParent;
+    Transform DragParent;
+  
+  private void Start() 
+    {
+        DragParent = GameObject.FindGameObjectWithTag("DragParent").transform;
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        pecaDragging = gameObject;
+        startPosition = transform.position;
+        startParent = transform.parent;
+        transform.SetParent(DragParent);
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+         Debug.Log("OnDrag");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        pecaDragging = null;
+        if(transform.parent == DragParent){
+            transform.position = startPosition;
+            transform.SetParent(startParent);
+
+        }
+
+    }
+    
+
+
+
+    private void Update()
+    {
+
+    }
+}
---
+++ Assets/Script/DropSlot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class DropSlot : MonoBehaviour, IDropHandler
+{
+       public GameObject peca;
+       public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("Drop");
+
+        if (!peca)
+        {
+            peca = DragHandLer.pecaDragging;
+            peca.transform.SetParent(transform);
+            peca.transform.position = transform.position;
+        }
+    }
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (peca != null && peca.transform.parent != transform)
+        {
+            Debug.Log("Remover");
+            peca = null;
+        }
+    }
+        
+    }
