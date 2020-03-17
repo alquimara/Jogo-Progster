9dfc4b5017d55caaac9e2943bf04f5ea7f1492ce
Alquimara Braz Alves <alquimara.a.braz@gmail.com>
2020-03-16 21:10:13 -0300

    Realizado ação de andar

--- Assets/Script/movimento.cs
+++ Assets/Script/movimento.cs
@@ -6,40 +6,47 @@ using UnityEngine.SceneManagement;
 public class movimento : MonoBehaviour
 {
     // Start is called before the first frame update
-    	public float velocidade;
+    public float velocidade;
 	public float posicao;
 	private Rigidbody2D personagem;
 	public float velocidadeY;
 	public GameObject other;
 	public Vector2 posicaoinicial;
-	
-
+	private Vector2 andar;
+ 	private List<string> acoes = new List<string>();
 
 	// Use this for initialization
 	void Start () {
 		personagem = GetComponent<Rigidbody2D> ();
-		print(transform.position.x);
-		
+		andar = Vector2.right* 1.45F;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.D)) {
-			transform.Translate(Vector2.right* 1.45F);
-			print(transform.position.x);
-			
-
-
-		
+		if (Input.GetKeyDown (KeyCode.A)) {
+			acoes.Add("andar");
 		}
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown (KeyCode.P)) {
+			StartCoroutine(Executar());
+		}
+		/*if (Input.GetKeyDown (KeyCode.Space)) {
 			personagem.AddForce (transform.up * 5 );
-			
+		}*/
+	}
 
-		
-		
+	public void Andar(){
+		transform.Translate(andar);
+	}
+
+	public IEnumerator Executar(){
+		foreach(string a in acoes){
+			if(a == "andar"){
+				Andar();
+			}
+			yield return new WaitForSeconds(0.5F);
 		}
 	}
+
 void OnTriggerEnter2D (Collider2D Obj){
 		if(Obj.gameObject.tag == "semente"){
 			Destroy (Obj.gameObject);
